5a6bbd0a560722bd4e648b187193463089ef1170
Laura Grassi <laura_grassig@outlook.com>
2020-01-15 20:51:42 -0300

    Start front-end

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './global.scss';
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+
+      </aside>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
