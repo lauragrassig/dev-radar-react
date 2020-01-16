37cb069af936243a379433729b7c51ecf0a157ba
Laura Grassi <laura_grassig@outlook.com>
2020-01-15 21:16:15 -0300

    front end sidebar style

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,46 @@
 import React, { useState } from 'react';
 import './global.scss';
+import './app.scss';
+import './sidebar.scss';
+
 
 function App() {
   return (
     <div id="app">
       <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
 
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+            
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/>
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+
+        </form>
       </aside>
-      <main></main>
+
+      <main>
+        <ul>
+          
+        </ul>
+      </main>
     </div>
   );
 }
