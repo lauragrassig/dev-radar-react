872e18784c9df7f8d20dd7a717feb7fba928e2b0
Laura Grassi <laura_grassig@outlook.com>
2020-01-16 21:04:08 -0300

    index js

--- web/src/components/DevItem/index.js
+++ web/src/components/DevItem/index.js
@@ -4,11 +4,12 @@ function DevItem({dev}) {
   return (
     <li className="dev-item">
             <header>
+              
               <img src={dev.avatar_url} alt={dev.name}/>
               <div className="user-info">
                 <strong>{dev.name}</strong>
                 <span>{dev.techs.join(', ')}</span>
-              </div>
+              </div> 
             </header>
             <p>{dev.bio}</p>
             <a href={`https://github.com/${dev.github_username}`}>Acessar Perfil no Github</a>
